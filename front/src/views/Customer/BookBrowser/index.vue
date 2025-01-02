a914ebb0996c24d352783cf8f9669547b1661960
HyperKindred <syu1692772@gmail.com>
2025-01-02 21:13:11 +0800

    shabi

--- front/src/views/Customer/BookBrowser/index.vue
+++ front/src/views/Customer/BookBrowser/index.vue
@@ -94,7 +94,7 @@ import VantaBirds from '../../VantaBirds.vue';
 import slide1 from "../../../assets/images/slide1.jpg"
 import slide2 from "../../../assets/images/slide2.jpg"
 import slide3 from "../../../assets/images/slide3.jpg"
-import slide4 from "../../../assets/images/slide4.jpg"
+import slide4 from "../../../assets/images/slide4.png"
 // 初始化 Pinia store
 const store = mainStore();
 const searchInput = ref(''); // 搜索输入框绑定的值
